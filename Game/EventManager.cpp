f4f83a452b5756059a80bf0fe05b2fc96ee15658
Jade <jade2.eames@live.uwe.ac.uk>
2023-02-20 15:32:00 +0000

    Singleton GameManager
    GameManager created as a singleton - needs extending.
    
    Base Manager class exists - might need added functionality later?
    
    Have also created a temporary EventManager - all it does is print "hi" (feel free to kill that).

---
+++ Game/EventManager.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "EventManager.h"
+#include "GameManager.h"
+
+EventManager::EventManager()
+{
+
+}
+
+EventManager::~EventManager()
+{
+
+}
+
+void EventManager::update()
+{
+	std::cout << "hi";
+}
