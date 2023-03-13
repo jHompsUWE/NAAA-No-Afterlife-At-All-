d948bc6f0e81ec24151cf7b9b330df1fc83d252a
Paul Erwich <paul2-erwich@live.uwe.uk>
2023-03-13 16:57:25 +0000

    Started commenting grid location

--- Game/GridLocation.h
+++ Game/GridLocation.h
@@ -4,11 +4,19 @@
 #include "TileGO.h"
 #include "GPGO.h"
 
-// This object houses grid data and the object for it
+/////////////////////////////////////////////////
+/// This Class houses the in-world tile object and an instance of the struct
+/// For all the behind the scenes data used in grid locations
+/// There is currently 
+/////////////////////////////////////////////////
 class GridLocation
 {
 public:
 	GridLocation();
+	/// \brief Constructor allowing for the creation of the location
+	/// \param _device Takes the DirectX3D11 device context
+	/// \param pos Grid coordinate for world positioning
+	/// \param plane The world plane used for positioning during world creation
 	GridLocation(Microsoft::WRL::ComPtr<ID3D11DeviceContext1> _device, Vector2 pos, int plane);
 	~GridLocation();
 
