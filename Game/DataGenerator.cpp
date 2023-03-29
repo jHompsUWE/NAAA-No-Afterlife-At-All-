0d7fdc068e931701bcd7168cddd2fd13c27eaa93
Luryann <lurianncervi@hotmail.com>
2023-03-29 23:34:39 +0100

    Random bio and name generator for souls

---
+++ Game/DataGenerator.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include "DataGenerator.h"
+
+#include "GameData.h"
+#include "GameManager.h"
+
+std::pair<std::string, std::string> DataGenerator::GenerateData()
+{
+    std::srand(std::time(nullptr));
+    auto dataList = GameManager::get()->getFileManagerV2()->GetJson("souls_name");    
+    
+    if (!dataList->empty())
+    {
+        string name =  (*dataList)["names"][std::rand() % 30];
+        string bio = GenerateBio(name, *dataList);
+        cout << bio << endl;
+        return std::make_pair(name, bio);        
+    }
+    return std::make_pair("", "");
+}
+
+std::string DataGenerator::GenerateBio(std::string& name, json& dataList)
+{
+    int hobbyIndex1, hobbyIndex2, hobbyIndex3;
+    do {
+        hobbyIndex1 = std::rand() % 40;
+        hobbyIndex2 = std::rand() % 40;
+        hobbyIndex3 = std::rand() % 40;
+    } while (hobbyIndex1 == hobbyIndex2 || hobbyIndex1 == hobbyIndex3 || hobbyIndex2 == hobbyIndex3);
+
+    string bio = name + " is a " + dataList["ages"][std::rand() % 82].get<std::string>() + " years-old " + dataList["jobs"][std::rand() % 40].get<std::string>() + " who enjoys " + dataList["hobbies"][hobbyIndex1].get<std::string>() + ", " + dataList["hobbies"][hobbyIndex2].get<std::string>() + ", and " + dataList["hobbies"][hobbyIndex3].get<std::string>();
+    return bio;
+}
---
+++ Game/DataGenerator.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <map>
+#include "FileManagerV2.h"
+
+class DataGenerator
+{
+public:
+    static std::pair<std::string, std::string> GenerateData();
+
+private:
+    static std::string GenerateBio(std::string& name, json& dataList);
+};
