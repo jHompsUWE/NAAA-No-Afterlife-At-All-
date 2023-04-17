4403bacc92df8b1af4214e5bd87cbc57c8780c0e
Luryann <lurianncervi@hotmail.com>
2023-04-17 13:58:34 +0100

    Initial files

---
+++ Game/BadThings.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "BadThings.h"
+
+void BadThings::triggerBadThing(BadThingsTypes _badThingType)
+{
+    //BadThingsVector_.push_back(_badThingType);
+}
+
+void BadThings::removeBadThing(BadThingsTypes _badThingType)
+{
+}
+
+void BadThings::update(GameData& _game_data)
+{
+    for (BadThingVector::const_iterator badThingIt = BadThingsVector_.begin(),
+       end = BadThingsVector_.end();
+        badThingIt != end;
+       ++badThingIt)
+    {
+        (*badThingIt)->update();
+    }
+}
---
+++ Game/BadThings.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include "GameManager.h"
+
+enum BadThingsTypes
+{
+    NONE,
+    BIRDS_OF_PARADISE,
+    BATS_OUT_OF_HELL,
+    HEAVEN_GETS_THE_BLUES,
+    DISCO_INFERNO,
+    PARADISE_PAIR_O_DANCE,
+    HELL_FREEZES_OVER,
+    HEAEN_NOSE,
+    HELL_IN_A_HANDBASKET,
+};
+
+class BadThing
+{
+public:
+    virtual ~BadThing() = default;
+    virtual void update();    
+};
+
+class BadThings : public Manager
+{    
+public:
+    void triggerBadThing(BadThingsTypes _badThingType);
+    void removeBadThing(BadThingsTypes _badThingType);
+    void update(GameData& _game_data) override;
+
+private:
+    typedef std::vector<BadThing*> BadThingVector;
+    BadThingVector BadThingsVector_;
+    
+    BadThingsTypes current_type_ = NONE;
+    
+};
